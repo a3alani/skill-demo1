f2446448720b66ca3d0820a52a674ee54b85840b
a3alani <a3.alani@gmail.com>
2022-05-11 14:48:48 -0700

    1

---
+++ skilldemo11Test.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skilldemo11Test {
+    @Test
+    public void multiplication() {
+    assertEquals(5, skilldemo11.multiply(2, 3));
+    }
+}
